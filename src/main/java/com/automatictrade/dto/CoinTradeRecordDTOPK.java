9115349b07b86be652d485fe521ce60aa4df1cfb
changsooooooooooo <lcs940621@gmail.com>
2021-09-30 17:19:00 +0900

    many to one one direction

--- src/main/java/com/automatictrade/dto/CoinTradeRecordDTOPK.java
+++ src/main/java/com/automatictrade/dto/CoinTradeRecordDTOPK.java
@@ -5,15 +5,19 @@ import lombok.Getter;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import java.io.Serializable;
 
 @Getter
 @Embeddable
 public class CoinTradeRecordDTOPK implements Serializable {
 
+    @ManyToOne
+    @JoinColumn(name="coin_name")
     @Column(name="code")
     @JsonProperty("cd")
-    private String coinType;
+    private CoinThemeDTO coinType;
 
     @Column(name="sequential_id")
     @JsonProperty("sid")
