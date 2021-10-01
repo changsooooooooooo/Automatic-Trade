0039b366465943be5294c003207c8d8d819e0d3d
changsooooooooooo <lcs940621@gmail.com>
2021-10-01 16:23:10 +0900

    two direction jpa updated

--- src/main/java/com/automatictrade/dto/CoinThemeDTOPK.java
+++ src/main/java/com/automatictrade/dto/CoinThemeDTOPK.java
@@ -2,13 +2,17 @@ package com.automatictrade.dto;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.OneToMany;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Embeddable
 public class CoinThemeDTOPK implements Serializable {
 
     @Column(name="coin_name")
-    private String coinName;
+    @OneToMany(mappedBy = "coinType")
+    private List<CoinTradeRecordDTO> coinTradeRecordDTOList = new ArrayList<>();
 
     @Column(name="coin_category")
     private String coinCategory;
